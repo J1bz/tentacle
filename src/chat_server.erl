c215a8868a50b6018a77a106a16dad0bff7fbcbc
Jean-Baptiste Braun <jbaptiste.braun@gmail.com>
2015-12-10 10:43:01 +0100

    Add some debug logs to chat_server

--- src/chat_server.erl
+++ src/chat_server.erl
@@ -14,11 +14,13 @@ start(Port) ->
 
 start_server(Port) ->
     {ok, ListeningSocket} = gen_tcp:listen(Port, ?TCP_OPTIONS),
-    register(server_pid, spawn(fun() -> server([]) end)),
+    register(server_pid, spawn(fun() -> server(init) end)),
     spawn(fun() -> user_connect(ListeningSocket) end).
 
+server(init) ->
+    io:format("Server main process started~n"),
+    server([]);
 server(Users) ->
-    io:format("Connected users: ~p~n", [Users]),
     receive
         {connect, Socket} ->
             New_Users = add_user(Socket, Users),
@@ -48,6 +50,9 @@ add_user(Connecting_Socket, Connected_Users) ->
         {ok, Connecting_Name} ->
             case lists:keymember(Connecting_Name, 2, Connected_Users) of
                 true ->
+                    io:format("User ~p wanted to connect to server, but it is "
+                              "already connected~n", [Connecting_Name]),
+                    io:format("Connected users: ~p~n", [Connected_Users]),
                     Connected_Users;
                 false ->
                     common:map(fun(Connected_User) ->
@@ -60,14 +65,22 @@ add_user(Connecting_Socket, Connected_Users) ->
                                end,
                                Connected_Users
                     ),
-                    [{Connecting_Socket, Connecting_Name} | Connected_Users]
+                    io:format("User ~p successfully connected to server~n",
+                              [Connecting_Name]),
+                    New_Users =  [{Connecting_Socket, Connecting_Name} |
+                                  Connected_Users],
+                    io:format("Connected users: ~p~n", [New_Users]),
+                    New_Users
             end;
         {error, Error} ->
             io:format("Error ~p~n", [Error])
     end.
 
 notify_presence(Name, Socket) ->
-    gen_tcp:send(Socket, common:format("Presence~n~s~n", [Name])).
+    Frame = common:format("Presence~n~s~n", [Name]),
+    %TODO: faire de ce socket un name
+    io:format("Sending frame ~p to socket ~p~n", [Frame, Socket]),
+    gen_tcp:send(Socket, Frame).
 
 rm_user(Socket, Users) ->
     % case au cas ou le serveur a recu une mauvaise info
@@ -76,16 +89,26 @@ rm_user(Socket, Users) ->
             Name = common:get_value(Socket, Users),
 
             New_Users = lists:keydelete(Socket, 1, Users),
-            common:map(fun(User) -> notify_absence(Name, User) end, New_Users),
+            common:map(fun(User) ->
+                        notify_absence(Name, User)
+                       end,
+                       New_Users),
+            io:format("User ~p successfully disconnected~n",
+                      [Name]),
+            io:format("Connected users: ~p~n", [New_Users]),
             New_Users;
         false ->
+            io:format("Socket ~p wanted to disconnect from server, but "
+                      "it is not in users list~n", [Socket]),
+            io:format("Connected users: ~p~n", [Users]),
             Users
     end.
 
-notify_absence(Formatted_Name, User) ->
-    {Notified_Socket, _} = User,
-    gen_tcp:send(Notified_Socket, common:format("Absence~n~s~n",
-                                                [Formatted_Name])).
+notify_absence(Name, User) ->
+    {Socket, _} = User,
+    Frame = common:format("Absence~n~s~n", [Name]),
+    io:format("Sending frame ~p to socket ~p~n", [Frame, Socket]),
+    gen_tcp:send(Socket, Frame).
 
 get_sockets(Users)                   -> get_sockets(Users, []).
 get_sockets([User | Users], Sockets) ->
@@ -124,35 +147,48 @@ user_connect(ListeningSocket) ->
 
     case common:socket_to_name(Socket) of
         {ok, Name} ->
-            io:format("Client ~s connected~n", [Name]),
+            %TODO: remplacer texte par Socket ~p submitted a connection to
+            %                          server
+            io:format("Client ~p connected~n", [Name]),
             server_pid ! {connect, Socket},
-            listen_user_socket(Socket);
+            listen_user_socket(Socket, Name);
         {error, Error} ->
             io:format("Error: ~p~n", [Error])
     end.
 
-listen_user_socket(Socket) ->
+listen_user_socket(Socket, Name) ->
     case gen_tcp:recv(Socket, 0) of
         {ok, Frame} ->
+            io:format("Received frame ~p from user ~p~n", [Frame, Name]),
             case common:parse_frame(Frame) of
                 {data, {Message}} ->
+                    io:format("Frame ~p has been detected as a data "
+                              "broadcast frame with message ~s~n",
+                              [Frame, Message]),
                     server_pid ! {broadcast, Socket, Message};
                 {data, {To_String, Message}} ->
+                    io:format("Frame ~p has been detected as a data "
+                              "message frame with message ~s to send to ~p~n",
+                              [Frame, Message, To_String]),
                     server_pid ! {message, Socket, Message, To_String};
                 {presence, Socket_String} ->
-                    io:format("Received a presence from ~s, but server is not "
-                              "supposed to receive this frame... ignoring~n",
-                              [Socket_String]);
+                    io:format("Frame ~p has been detected as a presence from "
+                              "~s, but server is not supposed to receive "
+                              "this frame... ignoring~n",
+                              [Frame, Socket_String]);
                 {absence, Socket_String} ->
-                    io:format("Received an absence from ~s, but server is not "
-                              "supposed to receive this frame... ignoring~n",
-                              [Socket_String]);
+                    io:format("Frame ~p has been detected as an absence from "
+                              "~s, but server is not supposed to receive "
+                              "this frame... ignoring~n",
+                              [Frame, Socket_String]);
                 {unkown, Frame} ->
-                    io:format("Received an unkown frame: ~s~n", [Frame])
+                    io:format("Frame ~p has not been recognized... ignoring~n",
+                              [Frame])
             end,
-            listen_user_socket(Socket);
+            listen_user_socket(Socket, Name);
         {error, closed} ->
-            io:format("Connection closed~n"),
+            %TODO: remplacer par Socket closed connection
+            io:format("Connection has been closed by user ~p~n", [Name]),
             server_pid ! {disconnect, Socket},
             ok
     end.
